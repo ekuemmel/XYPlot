c65f168211b367e6bec1a89d958566d69a10ba54
Eberhard Kuemmel <info@ewmksoft.de>
2018-07-04 21:07:41 +0200

    Upgraded gradle version

--- demo_android/app/src/main/java/de/ewmksoft/example/MainActivity.java
+++ demo_android/app/src/main/java/de/ewmksoft/example/MainActivity.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import de.ewmksoft.xyplot.driver.IXYGraphView;
 import de.ewmksoft.xyplot.driver.XYGraphView;
 import de.ewmksoft.xyplot.core.IXYPlot;
+
 import android.app.Activity;
 import android.os.Bundle;
 import android.os.Handler;
@@ -14,150 +15,157 @@ import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.Toast;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class MainActivity extends Activity implements Handler.Callback {
-	private static final int MSG_TAG_UPDATE = 1;
-	private static final int MSG_TAG_SAVE = 2;
-	private IDataStorage dataStorage;
-	private Thread dataThread;
-	private XYGraphView xyGraphView;
-	private Handler myHandler;
+    Logger logger = LoggerFactory.getLogger(MainActivity.class);
 
-	public MainActivity() {
-		super();
-		myHandler = new Handler(this);
-	}
+    private static final int MSG_TAG_UPDATE = 1;
+    private static final int MSG_TAG_SAVE = 2;
+    private IDataStorage dataStorage;
+    private Thread dataThread;
+    private XYGraphView xyGraphView;
+    private Handler myHandler;
 
-	/** Called when the activity is first created. */
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.main);
+    public MainActivity() {
+        super();
+        myHandler = new Handler(this);
+    }
 
-		xyGraphView = (XYGraphView) findViewById(R.id.xyPlot1);
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
 
-		IXYPlot xyPlot = xyGraphView.getXYPlot();
-		xyPlot.setFontSize(20, 30);
-		xyPlot.setAxisLabels(true);
-		xyPlot.setSaveButtonVisisble(true);
+        xyGraphView = (XYGraphView) findViewById(R.id.xyPlot1);
 
-		Button button1 = (Button) findViewById(R.id.button1);
-		Button button2 = (Button) findViewById(R.id.button2);
-		Button button3 = (Button) findViewById(R.id.button3);
-		button1.setOnClickListener(new OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				initGraph(1, false);
-			}
-		});
-		button2.setOnClickListener(new OnClickListener() {
+        IXYPlot xyPlot = xyGraphView.getXYPlot();
+        xyPlot.setFontSize(20, 30);
+        xyPlot.setAxisLabels(true);
+        xyPlot.setSaveButtonVisisble(true);
 
-			@Override
-			public void onClick(View v) {
-				initGraph(2, false);
-			}
-		});
-		button3.setOnClickListener(new OnClickListener() {
+        Button button1 = (Button) findViewById(R.id.button1);
+        Button button2 = (Button) findViewById(R.id.button2);
+        Button button3 = (Button) findViewById(R.id.button3);
+        button1.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                initGraph(1, false);
+            }
+        });
+        button2.setOnClickListener(new OnClickListener() {
 
-			@Override
-			public void onClick(View v) {
-				initGraph(3, false);
-			}
-		});
+            @Override
+            public void onClick(View v) {
+                initGraph(2, false);
+            }
+        });
+        button3.setOnClickListener(new OnClickListener() {
 
-		dataThread = new Thread(new Runnable() {
-			public void run() {
-				while (true) {
-					try {
-						Thread.sleep(100);
-						if (dataStorage != null) {
-							dataStorage.update();
-						}
-					} catch (InterruptedException e) {
-						break;
-					}
-				}
-			}
-		});
-		dataThread.start();
-	}
+            @Override
+            public void onClick(View v) {
+                initGraph(3, false);
+            }
+        });
 
-	@Override
-	public void onResume() {
-		super.onResume();
-	}
+        dataThread = new Thread(new Runnable() {
+            public void run() {
+                while (true) {
+                    try {
+                        Thread.sleep(100);
+                        if (dataStorage != null) {
+                            dataStorage.update();
+                        }
+                    } catch (InterruptedException e) {
+                        break;
+                    }
+                }
+            }
+        });
+        dataThread.start();
+    }
 
-	@Override
-	public void onDestroy() {
-		dataThread.interrupt();
-		try {
-			dataThread.join(2000);
-		} catch (InterruptedException e) {
-			// Ignore
-		}
-		super.onDestroy();
-	}
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
 
-	@Override
-	public boolean handleMessage(Message msg) {
-		switch (msg.what) {
-		case MSG_TAG_UPDATE:
-			if (dataStorage != null) {
-				dataStorage.updateXAxis(xyGraphView);
-				if (dataStorage.isEnabled()) {
-					myHandler.sendEmptyMessageDelayed(MSG_TAG_UPDATE, 100);
-				}
-			}
-			break;
-		case MSG_TAG_SAVE:
-			if (dataStorage != null) {
-				dataStorage.setEnabled(false);
-				try {
-					String result = dataStorage.save();
-					Toast.makeText(this, "Saved: " + result, Toast.LENGTH_SHORT)
-							.show();
-				} catch (IOException e) {
-					Toast.makeText(this, "Error on save: " + e.getMessage(),
-							Toast.LENGTH_SHORT).show();
-				}
-			}
-			break;
-		default:
-			break;
-		}
-		return true;
-	}
+    @Override
+    public void onDestroy() {
+        dataThread.interrupt();
+        try {
+            dataThread.join(2000);
+        } catch (InterruptedException e) {
+            // Ignore
+        }
+        super.onDestroy();
+    }
 
-	private void initGraph(int dataStorageNum, boolean loadPrevData) {
-		MyApplication application = (MyApplication) getApplication();
-		dataStorage = application.getDataStorage(dataStorageNum, this);
-		dataStorage.setEnabled(true);
-		if (loadPrevData) {
-			try {
-				dataStorage.restore();
-			} catch (IOException e) {
-				Toast.makeText(this, "Error: " + e.getMessage(),
-						Toast.LENGTH_SHORT).show();
-			}
-		}
-		xyGraphView.init("Time", "s", dataStorage.getDataHandlers(),
-				new IXYGraphView() {
-					public void setEnabled(boolean enabled) {
-						dataStorage.setEnabled(enabled);
-						myHandler.sendEmptyMessageDelayed(1, 100);
-					}
+    @Override
+    public boolean handleMessage(Message msg) {
+        switch (msg.what) {
+            case MSG_TAG_UPDATE:
+                if (dataStorage != null) {
+                    dataStorage.updateXAxis(xyGraphView);
+                    if (dataStorage.isEnabled()) {
+                        myHandler.sendEmptyMessageDelayed(MSG_TAG_UPDATE, 100);
+                    }
+                }
+                break;
+            case MSG_TAG_SAVE:
+                if (dataStorage != null) {
+                    dataStorage.setEnabled(false);
+                    try {
+                        String result = dataStorage.save();
+                        Toast.makeText(this, "Saved: " + result, Toast.LENGTH_SHORT)
+                                .show();
+                    } catch (IOException e) {
+                        Toast.makeText(this, "Error on save: " + e.getMessage(),
+                                Toast.LENGTH_SHORT).show();
+                    }
+                }
+                break;
+            default:
+                break;
+        }
+        return true;
+    }
 
-					public boolean isEnabled() {
-						return dataStorage.isEnabled();
-					}
+    private void initGraph(int dataStorageNum, boolean loadPrevData) {
+        MyApplication application = (MyApplication) getApplication();
+        dataStorage = application.getDataStorage(dataStorageNum, this);
+        dataStorage.setEnabled(true);
+        if (loadPrevData) {
+            try {
+                dataStorage.restore();
+            } catch (IOException e) {
+                Toast.makeText(this, "Error: " + e.getMessage(),
+                        Toast.LENGTH_SHORT).show();
+            }
+        }
+        xyGraphView.init("Time", "s", dataStorage.getDataHandlers(),
+                new IXYGraphView() {
+                    public void setEnabled(boolean enabled) {
+                        dataStorage.setEnabled(enabled);
+                        myHandler.sendEmptyMessageDelayed(1, 100);
+                    }
 
-					public void clear() {
-						dataStorage.clearData();
-					}
+                    public boolean isEnabled() {
+                        return dataStorage.isEnabled();
+                    }
 
-					public void save() {
-						myHandler.sendEmptyMessageDelayed(MSG_TAG_SAVE, 100);
-					}
-				});
-		myHandler.sendEmptyMessage(MSG_TAG_UPDATE);
-	}
+                    public void clear() {
+                        dataStorage.clearData();
+                    }
+
+                    public void save() {
+                        myHandler.sendEmptyMessageDelayed(MSG_TAG_SAVE, 100);
+                    }
+                });
+        myHandler.sendEmptyMessage(MSG_TAG_UPDATE);
+    }
 }
